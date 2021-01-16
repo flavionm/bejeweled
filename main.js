047d3a837fa60d75a987d48e9c58ad028df58d4f
Flávio Sousa <fn_maia@outlook.com>
2021-01-15 21:54:36 -0300

    Finish interface

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+"use strict";
+
 function main() {
-	console.log("Hello world!");
+	document.getElementById("game").innerHTML = "0 1 2 3 4 5 6 7\n\n"
+		+ "! $ + % # @ $ *    0\n"
+		+ "! * @ $ # % # !    1\n"
+		+ "% + % % ! # $ *    2\n"
+		+ "% % $ $ * * @ !    3\n"
+		+ "* @ % @ * ! $ $    4\n"
+		+ "+ $ @ # % + * #    5\n"
+		+ "* % ! * ! @ # $    6\n"
+		+ "# ! # @ @ $ ! *    7";
+
+	document.getElementById("score").innerHTML = "Score: 0";
+
+	document.getElementById("instructions").innerHTML = "Enter a position (row, column):";
 }
 
 main()
