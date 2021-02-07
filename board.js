38740d65a541c70f7c02534d34142c0dbee04500
Flávio Sousa <fn_maia@outlook.com>
2021-02-07 17:12:28 -0300

    Format code

--- board.js
+++ board.js
@@ -7,103 +7,103 @@ class Board {
 		this.board = new Array(size);
 		this.jewels = ["red", "green", "blue", "yellow", "purple"];
 		this.score = 0;
-		this.jewel_value=10;
+		this.jewel_value = 10;
 		this.display = display;
 		var id = 0;
 		for (var i = 0; i < this.size; i++) {
-			this.board[i]=Array(size);
-			for(var j=0;j<this.size;j++){
+			this.board[i] = Array(size);
+			for (var j = 0; j < this.size; j++) {
 				const jewel = document.createElement('div');
-    			jewel.setAttribute('draggable', true);
-    			jewel.setAttribute('id', id);
-    			id++;
-    			jewel.style.backgroundColor = this.jewels[Math.floor(Math.random() * this.jewels.length)];
-    			jewel.addEventListener('dragstart', dragStart)
-    			jewel.addEventListener('dragend', dragEnd);
+				jewel.setAttribute('draggable', true);
+				jewel.setAttribute('id', id);
+				id++;
+				jewel.style.backgroundColor = this.jewels[Math.floor(Math.random() * this.jewels.length)];
+				jewel.addEventListener('dragstart', dragStart)
+				jewel.addEventListener('dragend', dragEnd);
 				jewel.addEventListener('dragover', dragOver);
 				jewel.addEventListener('dragenter', dragEnter);
 				jewel.addEventListener('drageleave', dragLeave);
 				jewel.addEventListener('drop', dragDrop);
-    			this.display.appendChild(jewel);
-    			this.board[i][j]=jewel;
+				this.display.appendChild(jewel);
+				this.board[i][j] = jewel;
 			}
-    		
-  		}  				
+
+		}
 	}
-	
+
 	//Change the color of given jewel
-	changeBackColor(jewelId,colorBeingReplaced){
-		var row  = Math.floor(jewelId/this.size);
-		var column = jewelId%this.size;
-		this.board[row][column].style.backgroundColor=colorBeingReplaced;
+	changeBackColor(jewelId, colorBeingReplaced) {
+		var row = Math.floor(jewelId / this.size);
+		var column = jewelId % this.size;
+		this.board[row][column].style.backgroundColor = colorBeingReplaced;
 	}
-	
+
 	//Checks if the move made destroyed any pieces. Updates the score and returns the number of pieces destroyed;
-	checkMove(){
-		var num_jewels_destroyed=0;
+	checkMove() {
+		var num_jewels_destroyed = 0;
 		var jewel_selected;
 
-		for(var i=0;i<this.size;i++){
-			for(var j=0;j<this.size;j++){
+		for (var i = 0; i < this.size; i++) {
+			for (var j = 0; j < this.size; j++) {
 
-				jewel_selected=this.board[i][j].style.backgroundColor;
-				
+				jewel_selected = this.board[i][j].style.backgroundColor;
+
 				//condition to check if the jewel is already destroyed
-				if(jewel_selected != ""){
-					
+				if (jewel_selected != "") {
+
 					// check if 5 jewels are equal(horizontally)
-					if(j <= (this.size-5) && jewel_selected==this.board[i][j+1].style.backgroundColor && this.board[i][j+1].style.backgroundColor==this.board[i][j+2].style.backgroundColor
-						&& this.board[i][j+2].style.backgroundColor==this.board[i][j+3].style.backgroundColor && this.board[i][j+3].style.backgroundColor==this.board[i][j+4].style.backgroundColor){
-						num_jewels_destroyed+=5;
-						this.board[i][j].style.backgroundColor="";
-						this.board[i][j+1].style.backgroundColor="";
-						this.board[i][j+2].style.backgroundColor="";
-						this.board[i][j+3].style.backgroundColor="";
-						this.board[i][j+4].style.backgroundColor="";
+					if (j <= (this.size - 5) && jewel_selected == this.board[i][j + 1].style.backgroundColor && this.board[i][j + 1].style.backgroundColor == this.board[i][j + 2].style.backgroundColor
+						&& this.board[i][j + 2].style.backgroundColor == this.board[i][j + 3].style.backgroundColor && this.board[i][j + 3].style.backgroundColor == this.board[i][j + 4].style.backgroundColor) {
+						num_jewels_destroyed += 5;
+						this.board[i][j].style.backgroundColor = "";
+						this.board[i][j + 1].style.backgroundColor = "";
+						this.board[i][j + 2].style.backgroundColor = "";
+						this.board[i][j + 3].style.backgroundColor = "";
+						this.board[i][j + 4].style.backgroundColor = "";
 					}
 					// check if  4 jewels are equal(horizontally)
-					else if(j <= (this.size-4) && jewel_selected==this.board[i][j+1].style.backgroundColor && this.board[i][j+1].style.backgroundColor==this.board[i][j+2].style.backgroundColor
-						&& this.board[i][j+2].style.backgroundColor==this.board[i][j+3].style.backgroundColor){
-						num_jewels_destroyed+=4;
-						this.board[i][j].style.backgroundColor="";
-						this.board[i][j+1].style.backgroundColor="";
-						this.board[i][j+2].style.backgroundColor="";
-						this.board[i][j+3].style.backgroundColor="";
+					else if (j <= (this.size - 4) && jewel_selected == this.board[i][j + 1].style.backgroundColor && this.board[i][j + 1].style.backgroundColor == this.board[i][j + 2].style.backgroundColor
+						&& this.board[i][j + 2].style.backgroundColor == this.board[i][j + 3].style.backgroundColor) {
+						num_jewels_destroyed += 4;
+						this.board[i][j].style.backgroundColor = "";
+						this.board[i][j + 1].style.backgroundColor = "";
+						this.board[i][j + 2].style.backgroundColor = "";
+						this.board[i][j + 3].style.backgroundColor = "";
 					}
 					// check if 3 jewels are equal(horizontally)
-					else if(j <= (this.size-3) && jewel_selected==this.board[i][j+1].style.backgroundColor 
-						&& this.board[i][j+1].style.backgroundColor==this.board[i][j+2].style.backgroundColor){
-						num_jewels_destroyed+=3;
-						this.board[i][j].style.backgroundColor="";
-						this.board[i][j+1].style.backgroundColor="";
-						this.board[i][j+2].style.backgroundColor="";
-					}				
+					else if (j <= (this.size - 3) && jewel_selected == this.board[i][j + 1].style.backgroundColor
+						&& this.board[i][j + 1].style.backgroundColor == this.board[i][j + 2].style.backgroundColor) {
+						num_jewels_destroyed += 3;
+						this.board[i][j].style.backgroundColor = "";
+						this.board[i][j + 1].style.backgroundColor = "";
+						this.board[i][j + 2].style.backgroundColor = "";
+					}
 					// check if 5 jewels are equal(vertically)
-					if(i <= (this.size-5) && jewel_selected==this.board[i+1][j].style.backgroundColor && this.board[i+1][j].style.backgroundColor==this.board[i+2][j].style.backgroundColor
-						&& this.board[i+2][j].style.backgroundColor==this.board[i+3][j].style.backgroundColor && this.board[i+3][j].style.backgroundColor==this.board[i+4][j].style.backgroundColor){
-						num_jewels_destroyed+=5;
-						this.board[i][j].style.backgroundColor="";
-						this.board[i+1][j].style.backgroundColor="";
-						this.board[i+2][j].style.backgroundColor="";
-						this.board[i+3][j].style.backgroundColor="";
-						this.board[i+4][j].style.backgroundColor="";
+					if (i <= (this.size - 5) && jewel_selected == this.board[i + 1][j].style.backgroundColor && this.board[i + 1][j].style.backgroundColor == this.board[i + 2][j].style.backgroundColor
+						&& this.board[i + 2][j].style.backgroundColor == this.board[i + 3][j].style.backgroundColor && this.board[i + 3][j].style.backgroundColor == this.board[i + 4][j].style.backgroundColor) {
+						num_jewels_destroyed += 5;
+						this.board[i][j].style.backgroundColor = "";
+						this.board[i + 1][j].style.backgroundColor = "";
+						this.board[i + 2][j].style.backgroundColor = "";
+						this.board[i + 3][j].style.backgroundColor = "";
+						this.board[i + 4][j].style.backgroundColor = "";
 					}
 					// check if 4 jewels are equal(vertically)
-					else if(i <= (this.size-4) && jewel_selected==this.board[i+1][j].style.backgroundColor && this.board[i+1][j].style.backgroundColor==this.board[i+2][j].style.backgroundColor
-						&& this.board[i+2][j].style.backgroundColor==this.board[i+3][j].style.backgroundColor){
-						num_jewels_destroyed+=4;
-						this.board[i][j].style.backgroundColor="";
-						this.board[i+1][j].style.backgroundColor="";
-						this.board[i+2][j].style.backgroundColor="";
-						this.board[i+3][j].style.backgroundColor="";
+					else if (i <= (this.size - 4) && jewel_selected == this.board[i + 1][j].style.backgroundColor && this.board[i + 1][j].style.backgroundColor == this.board[i + 2][j].style.backgroundColor
+						&& this.board[i + 2][j].style.backgroundColor == this.board[i + 3][j].style.backgroundColor) {
+						num_jewels_destroyed += 4;
+						this.board[i][j].style.backgroundColor = "";
+						this.board[i + 1][j].style.backgroundColor = "";
+						this.board[i + 2][j].style.backgroundColor = "";
+						this.board[i + 3][j].style.backgroundColor = "";
 					}
 					// check if 3 jewels are equal(vertically)
-					else if(i <= (this.size-3) && jewel_selected==this.board[i+1][j].style.backgroundColor 
-						&& this.board[i+1][j].style.backgroundColor==this.board[i+2][j].style.backgroundColor){
-						num_jewels_destroyed+=3;
-						this.board[i][j].style.backgroundColor="";
-						this.board[i+1][j].style.backgroundColor="";
-						this.board[i+2][j].style.backgroundColor="";
+					else if (i <= (this.size - 3) && jewel_selected == this.board[i + 1][j].style.backgroundColor
+						&& this.board[i + 1][j].style.backgroundColor == this.board[i + 2][j].style.backgroundColor) {
+						num_jewels_destroyed += 3;
+						this.board[i][j].style.backgroundColor = "";
+						this.board[i + 1][j].style.backgroundColor = "";
+						this.board[i + 2][j].style.backgroundColor = "";
 					}
 				}
 
@@ -111,29 +111,29 @@ class Board {
 		}
 
 		//updating the score
-		this.score+=this.jewel_value*num_jewels_destroyed;
-		return num_jewels_destroyed;		
+		this.score += this.jewel_value * num_jewels_destroyed;
+		return num_jewels_destroyed;
 	}
 
 	//Create new pieces
-	createPieces(){
-		for(var i=0; i < this.size;i++){
-			for(var j=0;j < this.size;j++){				
-				//for each piece on top of a cleared piece move it down one position 
+	createPieces() {
+		for (var i = 0; i < this.size; i++) {
+			for (var j = 0; j < this.size; j++) {
+				//for each piece on top of a cleared piece move it down one position
 				//or create a new one if it is on the first row
-				if(this.board[i][j].style.backgroundColor == ""){
-					if(i > 0){
-						for(var k=i;k >= 0;k--){
-							if(k > 0){
-								this.board[k][j].style.backgroundColor=this.board[k-1][j].style.backgroundColor;	
+				if (this.board[i][j].style.backgroundColor == "") {
+					if (i > 0) {
+						for (var k = i; k >= 0; k--) {
+							if (k > 0) {
+								this.board[k][j].style.backgroundColor = this.board[k - 1][j].style.backgroundColor;
 							}
-							else{
-								this.board[k][j].style.backgroundColor=this.jewels[Math.floor(Math.random() * 5)];	
-							} 								
-						}						
+							else {
+								this.board[k][j].style.backgroundColor = this.jewels[Math.floor(Math.random() * 5)];
+							}
+						}
 					}
-					else{
-						this.board[i][j].style.backgroundColor=this.jewels[Math.floor(Math.random() * 5)];
+					else {
+						this.board[i][j].style.backgroundColor = this.jewels[Math.floor(Math.random() * 5)];
 					}
 				}
 			}
--- main.js
+++ main.js
@@ -10,21 +10,21 @@ var jewelIdBeingDragged = null;
 var jewelIdBeingReplaced = null;
 
 //flag to check if a valid moved destroyed any pieces
-var piece_destr=false;
+var piece_destr = false;
 //timer of setInterval
-var timer= 400;
+var timer = 400;
 
 
 // Initialize game elements.
 function start() {
-	const grid = document.querySelector('.grid')
-	board = new Board(size, grid);
-	document.getElementById("score").innerHTML = board.score;
-	//document.getElementById("row").setAttribute("max", size - 1);
-	//document.getElementById("column").setAttribute("max", size - 1);
+    const grid = document.querySelector('.grid')
+    board = new Board(size, grid);
+    document.getElementById("score").innerHTML = board.score;
+    //document.getElementById("row").setAttribute("max", size - 1);
+    //document.getElementById("column").setAttribute("max", size - 1);
 }
 
-function dragStart(){
+function dragStart() {
     colorBeingDragged = this.style.backgroundColor
     jewelIdBeingDragged = parseInt(this.id)
 }
@@ -45,46 +45,46 @@ function dragDrop() {
     colorBeingReplaced = this.style.backgroundColor
     jewelIdBeingReplaced = parseInt(this.id)
     this.style.backgroundColor = colorBeingDragged
-    board.changeBackColor(jewelIdBeingDragged,colorBeingReplaced)
+    board.changeBackColor(jewelIdBeingDragged, colorBeingReplaced)
 }
 
 function dragEnd() {
-    
-    var validMoves = [jewelIdBeingDragged-1 , jewelIdBeingDragged-size, jewelIdBeingDragged+1, jewelIdBeingDragged+size]
+
+    var validMoves = [jewelIdBeingDragged - 1, jewelIdBeingDragged - size, jewelIdBeingDragged + 1, jewelIdBeingDragged + size]
     var validMove = validMoves.includes(jewelIdBeingReplaced)
-    
+
     //checking for a valid move
     if (jewelIdBeingReplaced && validMove) {
-        piece_destr=true;
-    }  else if (jewelIdBeingReplaced && !validMove) {
-    	board.changeBackColor(jewelIdBeingReplaced,colorBeingReplaced)
-    	board.changeBackColor(jewelIdBeingDragged,colorBeingDragged)
-    } else board.changeBackColor(jewelIdBeingDragged,colorBeingDragged)
+        piece_destr = true;
+    } else if (jewelIdBeingReplaced && !validMove) {
+        board.changeBackColor(jewelIdBeingReplaced, colorBeingReplaced)
+        board.changeBackColor(jewelIdBeingDragged, colorBeingDragged)
+    } else board.changeBackColor(jewelIdBeingDragged, colorBeingDragged)
 }
 
-function checkBoard(){
-	
-	var points=board.checkMove();
-	//checking if move destroyed any pieces
-	if( points > 0 && piece_destr){
-		 piece_destr=false;	
-	}
-	else if(jewelIdBeingReplaced!= null && jewelIdBeingDragged!=null && piece_destr){
-		board.changeBackColor(jewelIdBeingReplaced,colorBeingReplaced)
-    	board.changeBackColor(jewelIdBeingDragged,colorBeingDragged)
-	}
+function checkBoard() {
+
+    var points = board.checkMove();
+    //checking if move destroyed any pieces
+    if (points > 0 && piece_destr) {
+        piece_destr = false;
+    }
+    else if (jewelIdBeingReplaced != null && jewelIdBeingDragged != null && piece_destr) {
+        board.changeBackColor(jewelIdBeingReplaced, colorBeingReplaced)
+        board.changeBackColor(jewelIdBeingDragged, colorBeingDragged)
+    }
 }
 
-function createPieces(){
-	board.createPieces();
+function createPieces() {
+    board.createPieces();
 }
 
-function updateScore(){
-	document.getElementById("score").innerHTML = board.score;
+function updateScore() {
+    document.getElementById("score").innerHTML = board.score;
 }
 
-window.setInterval(function(){
+window.setInterval(function () {
     checkBoard();
     createPieces();
     updateScore();
-  }, timer);
+}, timer);
